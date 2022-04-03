bffea910558e317ee3677f7f5f168c5374840da9
Savics407 <chibuvic135@gmail.com>
2022-04-03 23:42:01 +0100

    view corrections on linking

--- components/features.js
+++ components/features.js
@@ -7,7 +7,7 @@ class Features extends HTMLElement {
       this.innerHTML = `
        <div class="features">
                    <div class="sec-header">
-                       <h1 class="heading">The Best Talent <span class="span">Projection Platform</span></h1> 
+                       <h1 class="heading">Best <span class="span">Edutainment </span>Platform</h1> 
                        <p class="heading-p">Providing Services and opportunities Scalable for the entire globe</p>
                     </div>
                     <div class="featured-blocks">
@@ -15,7 +15,7 @@ class Features extends HTMLElement {
                             <div class="feat-content">
                                 <p> <span class="feat-title"> Talent Mining</span></p>
                                 <p>We can help you discover the talent buried within you.</p>
-                                <button>Learn More &nbsp<i class="fas fa-angle-right "></i></button>
+                                <a href="about.html"><button>Learn More &nbsp<i class="fas fa-angle-right "></i></button></a>
                             </div>
                            
                         </div>
@@ -23,14 +23,14 @@ class Features extends HTMLElement {
                             <div class="feat-content">
                                 <p> <span class="feat-title"> Talent Refining</span></p>
                                 <p>We help reform your talents</p>
-                                <button>Learn More &nbsp<i class="fas fa-angle-right "></i></button>
+                                <a href="about.html"><button>Learn More &nbsp<i class="fas fa-angle-right "></i></button></a>
                             </div>
                         </div>
                         <div class="featured feat-bg3">
                             <div class="feat-content">
                                 <p> <span class="feat-title"> Talent Projection</span></p>
                                 <p>We create an enabling environment to showcase your talent and get it to the spotlight</p>
-                                <button>Learn More &nbsp<i class="fas fa-angle-right "></i></button>
+                                <a href="about.html"><button>Learn More &nbsp<i class="fas fa-angle-right "></i></button></a>           
                             </div>
                            
                         </div>
